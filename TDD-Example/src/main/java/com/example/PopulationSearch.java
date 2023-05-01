6600832775669035e0ccf174546c35241c27886b
Nagarjuna Pottakarla <nagarjuna9189@gmail.com>
2023-05-01 20:35:47 +0530

    "Population Example"

---
+++ TDD-Example/src/main/java/com/example/PopulationSearch.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PopulationSearch {
+
+	public int getPopulation(String city) {
+		Map<String, Integer> populationMap=null;
+		int populationCount=0;
+		if(city.isEmpty()) {
+			throw new NullPointerException("input should be  Empty:");
+		}
+		populationMap= getPopulationStatistic();
+		if(!populationMap.containsKey(city)) {
+			throw new NullPointerException("invalid input:");
+		}else {
+		populationCount= populationMap.get(city);
+		}
+		return populationCount;
+	}
+
+	private Map<String, Integer> getPopulationStatistic() {
+		Map<String, Integer> populationMap = new HashMap<String, Integer>();
+		populationMap.put("Bangalore", 200000);
+		populationMap.put("Hyderabad", 500000);
+		populationMap.put("Pune", 300000);
+		populationMap.put("Mumbai", 800000);
+
+		return populationMap;
+
+	}
+
+}
---
+++ TDD-Example/src/test/java/PopulationSearchTest.java
@@ -0,0 +1,40 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.example.PopulationSearch;
+
+public class PopulationSearchTest {
+	
+	private PopulationSearch ps;
+	
+	@Before
+	public void setup() {
+		ps=new PopulationSearch();
+	}
+
+	@Test
+	public void findPopulation() {
+		String city="Bangalore";
+		int expectedPopulation=200000;
+		//PopulationSearch ps=new PopulationSearch();
+		int count=ps.getPopulation(city);
+		Assert.assertEquals(expectedPopulation, count);
+}
+	@Test(expected =NullPointerException.class)
+	public void findPopulationEmptyInput() {
+		String city="";
+		int expectedPopulation=0;
+		//PopulationSearch ps=new PopulationSearch();
+		int count=ps.getPopulation(city);
+		Assert.assertEquals(expectedPopulation, count);
+}
+	@Test(expected =NullPointerException.class)
+	public void findPopulationInvalidInput() {
+		String city="xyz";
+		int expectedPopulation=0;
+		//PopulationSearch ps=new PopulationSearch();
+		int count=ps.getPopulation(city);
+		Assert.assertEquals(expectedPopulation, count);
+}
+}
